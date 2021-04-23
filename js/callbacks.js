71e9f78ce2b5dbf9f33f4127108726da1dd34f51
johanperjulius1 <johan.per.julius.berglund@gmail.com>
2021-04-23 14:07:46 +0200

    Removing button with cleanup.

--- js/callbacks.js
+++ js/callbacks.js
@@ -25,35 +25,43 @@ function getProfiles(json) {
   const profiles = json.people.map((person) => {
     return getJSON(wikiUrl + person.name);
   });
-  return profiles;
+  return Promise.all(profiles);
 }
 
 // Generate the markup for each profile
 function generateHTML(data) {
-  const section = document.createElement("section");
-  peopleList.appendChild(section);
-  // Check if request returns a 'standard' page from Wiki
-  if (data.type === "standard") {
-    section.innerHTML = `
-      <img src=${data.thumbnail.source}>
-      <h2>${data.title}</h2>
-      <p>${data.description}</p>
-      <p>${data.extract}</p>
-    `;
-  } else {
-    section.innerHTML = `
-      <img src="img/profile.jpg" alt="ocean clouds seen from space">
-      <h2>${data.title}</h2>
-      <p>Results unavailable for ${data.title}</p>
-      ${data.extract_html}
-    `;
-  }
+  data.map((person) => {
+    const section = document.createElement("section");
+    peopleList.appendChild(section);
+    // Check if request returns a 'standard' page from Wiki
+    if (person.type === "standard") {
+      section.innerHTML = `
+        <img src=${person.thumbnail.source}>
+        <h2>${person.title}</h2>
+        <p>${person.description}</p>
+        <p>${person.extract}</p>
+      `;
+    } else {
+      section.innerHTML = `
+        <img src="img/profile.jpg" alt="ocean clouds seen from space">
+        <h2>${person.title}</h2>
+        <p>Results unavailable for ${person.title}</p>
+        ${person.extract_html}
+      `;
+    }
+  });
 }
 
 btn.addEventListener("click", (event) => {
+  event.target.textContent = "Loading...";
+
   getJSON(astrosUrl)
     .then(getProfiles)
-    .then( data => console.log(data))
-    .catch( err => console.log(err));
+    .then(generateHTML)
+    .catch(err => {
+      peopleList.innerHTML = '<h3>something went wrong</h3>'
+      console.log(err)
+    })
+    .finally(() => event.target.remove());
   event.target.remove();
 });
